c5548ea342eba430520963fd6b5fead25f1da2d5
jen67 <amakrigift2000@gmail.com>
2023-10-07 14:11:17 -0700

    done

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
-"""
-function that retrieves an element from a list like in C.
-"""
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to retrieve an element from a list like in C
+#
+
 def element_at(my_list, idx):
+    """Retrieves an element from a list like in C
+
+    Args:
+        my_list: a list
+        idx: the index of item to retrieve
+
+    Returns:
+        the item at index idx
+    """
+
+    # Check for negative and out of range index
     if idx < 0:
         return None
     if idx >= len(my_list):
