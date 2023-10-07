df3b13dc29127f8c064e79669034ff825901661a
jen67 <amakrigift2000@gmail.com>
2023-10-07 14:48:54 -0700

    done

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,16 +1,5 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    """Replace an element at a specified index in a list with a new element.
-    
-    Args:
-    - my_list: The list in which the element will be replaced.
-    - idx: The index of the element to be replaced.
-    - element: The new element to insert at the specified index.
-    
-    Returns:
-    - The updated list.
-    """
-    
+
 def replace_in_list(my_list, idx, element):
     if idx >= 0 and idx < len(my_list):
         my_list[idx] = element
