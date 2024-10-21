202dc3177e6d4fa20fcb478bfb9fd923b1586167
jen67 <amakrigift2000@gmail.com>
2024-10-21 06:01:40 -0700

    task 5 done

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const args = process.argv;
+
+if (args.length < 3) {
+    console.log("Not a number");
+} else {
+    const firstArg = Number(args[2]); // Convert the first argument to a number
+
+    if (isNaN(firstArg)) {
+        console.log("Not a number");
+    } else {
+        console.log(`My number: ${firstArg}`);
+    }
+}
+
